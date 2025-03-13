b9423126b3aaa67b510e0c642f6be92fd77b16bf
blackjac7 <febriansyah705@gmail.com>
2025-03-13 23:24:18 +0700

    config database & env

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
+
+app.use("*", (_, res) => {
+  res.status(404).json({
+    data: null,
+    message: "Route not found",
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
